eb77b126ee6d0a88b03fbe54fefcd3da2e7b312c
Fazmis <vlad.par.2004@gmail.com>
2026-07-21 00:34:40 +0300

    feat(ecs): add entity lifecycle management

--- ecs/component_manager.py
+++ ecs/component_manager.py
@@ -6,24 +6,50 @@ class ComponentManager:
     def add_entity(self, entity_id:int, components:set|list):
         self.entity_components[entity_id] = components
         for component in components:
+            component.entity_id = entity_id
             self._add_component(component, entity_id)
 
     def _add_component(self, component, entity_id):
-        if type(component) not in self.component_entities:
-            self.component_entities[type(component)] = dict()
-        self.component_entities[type(component)][entity_id] = component
+        component_type = type(component)
+        if component_type not in self.component_entities:
+            self.component_entities[component_type] = dict()
+        if entity_id in self.component_entities[component_type]:
+            raise ValueError(f"Entity {entity_id} already has component {component_type.__name__}")
+        self.component_entities[component_type][entity_id] = component
 
-    def query(self, *components_types) -> list[list] | None:
+    def query(self, *components_types) -> list | list[list] | None:
         if not components_types:
             return None
+
+        for components_type in components_types:
+            if components_type not in self.component_entities:
+                return None
 
         keys = self.component_entities[components_types[0]].keys()
         for component in components_types[1:]:
             keys &= self.component_entities[component].keys()
+
         query = []
-        for key in keys:
-            selected_components = []
-            for component in components_types:
-                selected_components.append(self.component_entities[component][key])
-            query.append(selected_components)
+        if len(components_types) > 1:
+            for key in keys:
+                selected_components = []
+                for component in components_types:
+                    selected_components.append(self.component_entities[component][key])
+                query.append(selected_components)
+        else:
+            for key in keys:
+                query.append(self.component_entities[components_types[0]][key])
         return query
+
+    def delete_entity(self, item):
+        entity_id = None
+        if type(item) in self.component_entities:
+            entity_id = item.entity_id
+        elif item in self.entity_components:
+            entity_id = item
+        else:
+            raise ValueError(f"No entity id or component was found matching '{item}'.")
+        components = self.entity_components[entity_id]
+        self.entity_components.pop(entity_id)
+        for component in components:
+            self.component_entities[type(component)].pop(entity_id)
